03b58e1533b69be90ab9a230be1ec79afb8d838a
abhimanyu-siwach <siwachbahi@gmail.com>
2015-07-01 16:37:46 +0530

    Added Images Support

--- app/views/js/addTemplateController.js
+++ app/views/js/addTemplateController.js
@@ -1,6 +1,5 @@
-bbTemplates.controller("addTemplateController", function($scope, $http, $location){
-  var url = "/api/templates/";
-  $scope.url = url;
+bbTemplates.controller("addTemplateController", function($scope, $http, $window, $location){
+  var url = "/api/templates";
   $scope.post = function(){
     $http({
       method: 'POST',
@@ -9,10 +8,32 @@ bbTemplates.controller("addTemplateController", function($scope, $http, $locatio
       headers: {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'}
     }).success(function(response){
       if(response.message === "Template created!"){
-        $location.path("/");
+        upload(response.id, function(res){
+          console.log(res);
+          if(res === "Image uploaded!"){
+            $window.location.reload();
+            $location.path("/");
+          }else{
+            alert(res);
+          }
+        });
       }else{
         alert(response);
       }
+    });
+  };
+
+  var upload = function(id, callback){
+    var file = $scope.myFile;
+    var url = "/api/templates/" + id + "/image";
+    var fd = new FormData();
+    fd.append('file', file);
+    $http.post(url, fd, {
+        transformRequest: angular.identity,
+        headers: {'Content-Type': undefined}
+    })
+    .success(function(response){
+      callback(response)
     });
   };
 });
--- app/views/js/viewTemplateController.js
+++ app/views/js/viewTemplateController.js
@@ -1,4 +1,4 @@
-bbTemplates.controller("viewTemplateController", function($scope, $http, $routeParams, $location){
+bbTemplates.controller("viewTemplateController", function($scope, $http, $routeParams, $window, $location){
   var url = "/api/templates/" + $routeParams.id;
   $http.get(url).success(function(response){
     $scope.template = response;
@@ -11,6 +11,7 @@ bbTemplates.controller("viewTemplateController", function($scope, $http, $routeP
       headers: {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'}
     }).success(function(response){
       if(response.message === "Template updated!"){
+        $window.location.reload();
         $location.path("/");
       }else{
         alert(response);
@@ -20,7 +21,7 @@ bbTemplates.controller("viewTemplateController", function($scope, $http, $routeP
   $scope.delete = function(){
     $http.delete(url).success(function(response){
       if(response.message === "Template deleted!"){
-        $location.path("/");
+        $window.location.reload("/");
       }else{
         alert(response);
       }
