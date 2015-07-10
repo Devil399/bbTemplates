aec1169b0e28586d854517a1e8df3d044f408a9d
Austin <devil399@gmail.com>
2015-07-10 18:28:01 +0530

    Date filters on data table

--- app/views/js/searchController.js
+++ app/views/js/searchController.js
@@ -18,10 +18,52 @@ angular.module('bbTemplates', []).controller('searchController', function($scope
 
 
 
+    //function for custom filters on data Table
+    $.fn.dataTableExt.afnFiltering.push(
+            function( oSettings, aData, iDataIndex ) {
+                //Getting start an end date
+                var sdt = document.getElementById('startDate').value; //start date
+                var edt = document.getElementById('endDate').value; //end date
+                var createddt = aData[4]; //Created on, data on 4th column
+
+                if(sdt === "") var dt1=sdt //initially no date defined
+                    else var dt1 = parseDt(sdt);
+                if(edt === "")var dt2 = edt
+                    else var dt2 = parseDt(edt);
+                var dt = parseDt2(createddt); 
+                
+                if ( dt1 == "" && dt <= dt2){
+                    return true; //start date not given bt end date given
+                }
+                else if ( dt1 =="" && "" <= dt2 ){
+                    return true;
+                }
+                else if ( dt1 <= dt && "" == dt2 ){
+                    return true;
+                }
+                else if ( dt1 <= dt && dt <= dt2 ){
+                    return true;
+                }
+                return false;
+            }
+        );
+
+    function parseDt(rawDate){ 
+        var dateArr = rawDate.split('/');
+        var parsedDate = dateArr[0] + dateArr[1] + dateArr[2];
+        return parsedDate;
+    }
+    function parseDt2(rawDate){
+        var dateArr = rawDate.split('-');
+        var parsedDate = dateArr[0] + dateArr[1] + dateArr[2];
+        return parsedDate;
+    }
+
+
     $(document).ready( function () {
-      var table = $('#searchTable').DataTable({
+      table = $('#searchTable').DataTable({
         data: $scope.templates,
-        columns: [
+        columns: [ //show the following columns
             { data: 'name' },
             { data: 'price' },
             { data: 'url' },
@@ -31,15 +73,13 @@ angular.module('bbTemplates', []).controller('searchController', function($scope
             { data: 'dislikes' }
         ],
         dom: 'T<"clear">lfrtip',
-        tableTools: {
-            "sSwfPath": "./DataTables-1.10.7/extensions/TableTools/swf/copy_csv_xls_pdf.swf"
+        tableTools: { //tools for csv,excel,pdf....
+            "sSwfPath": "./libs/dataTable/extensions/TableTools/swf/copy_csv_xls_pdf.swf"
         }
       });
-    });
-    $.fn.dataTableExt.afnFiltering.push(function( settings, data, dataIndex ) {
-        function(oSettings, aData, iDataIndex){
-            alert('here');
-        }
+
+      $('#startDate').keyup( function() { table.draw(); } ); //redraw table on changing start/end dates
+      $('#endDate').keyup( function() {table.draw(); } );
     });
   });
 
